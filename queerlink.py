f9b5e991be1645bde0dea0f41ed938014937e451
EtienneDesticourt <etienne.desticourt@github.com>
2015-11-23 22:37:58 +0100

    Clean up

--- queerlink.py
+++ queerlink.py
@@ -4,33 +4,30 @@ import json
 
 HOST = "http://hemochro.me"
 
+def parseResponse(data):
+    strData = data.decode('ascii')
+    jsonData = json.loads(strData)
+    if jsonData['status'] == 'error':
+        raise HTTPError(fullUrl, 400, jsonData['data'], None, None)
+    return jsonData['data']
+
 def shorten(url, host=HOST):
     "Takes an url and returns a shortened."
     #Request shortened url
     fullUrl = host + "/shorten/json?url=" + url
     request = urllib.request.Request(fullUrl, method='PUT')
-    result = urllib.request.urlopen(request)
+    response = urllib.request.urlopen(request)
     #Parse the answer
-    if result.status == 200:
-        binData = result.read()
-        strData = binData.decode('ascii')
-        jsonData = json.loads(strData)
-        if jsonData['status'] == 'error':
-            raise HTTPError(fullUrl, 400, jsonData['data'], None, None)
-        return jsonData['data']
+    data = response.read()
+    return parseResponse(data)
 
 def expand(urlId, host=HOST):
     "Takes the ID of a shortened url and retrieves the original."
     #Request shortened url
     fullUrl = host + "/expand/json/" + urlId
     request = urllib.request.Request(fullUrl, method='GET')
-    result = urllib.request.urlopen(request)
+    response = urllib.request.urlopen(request)
     #Parse the answer
-    if result.status == 200:
-        binData = result.read()
-        strData = binData.decode('ascii')
-        jsonData = json.loads(strData)
-        if jsonData['status'] == 'error':
-            raise HTTPError(fullUrl, 400, jsonData['data'], None, None)
-        return jsonData['data']
+    data = response.read()
+    return parseResponse(data)
     
