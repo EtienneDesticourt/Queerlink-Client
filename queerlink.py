cc07dc6768c0c5f168f40662076ffc54da72c13f
EtienneDesticourt <etienne.desticourt@github.com>
2015-11-23 23:50:06 +0100

    Clean up

--- queerlink.py
+++ queerlink.py
@@ -4,32 +4,28 @@ import json
 
 HOST = "http://hemochro.me"
 
-def parseResponse(data):
+def fetchAndParse(request):
+    response = urllib.request.urlopen(request)   
+    data = response.read()
     strData = data.decode('ascii')
     jsonData = json.loads(strData)
     if jsonData['status'] == 'error':
-        raise HTTPError(fullUrl, 400, jsonData['data'], None, None)
+        raise HTTPError(request.get_full_url(), 400, jsonData['data'], None, None)
     return jsonData['data']
 
 def shorten(url, host=HOST):
-    "Takes a url and returns a shortened."
+    "Takes a url and returns a shortened url."
     #Request shortened url
     fullUrl = host + "/shorten/json?url=" + url
     request = urllib.request.Request(fullUrl, method='PUT')
-    response = urllib.request.urlopen(request)
-    #Parse the answer
-    data = response.read()
-    return parseResponse(data)
+    return fetchAndParse(request)
 
 def expand(urlId, host=HOST):
     "Takes the ID of a shortened url and retrieves the original."
     #Request shortened url
     fullUrl = host + "/expand/json/" + urlId
     request = urllib.request.Request(fullUrl, method='GET')
-    response = urllib.request.urlopen(request)
-    #Parse the answer
-    data = response.read()
-    return parseResponse(data)
+    return fetchAndParse(request)
     
 if __name__ == "__main__":
     import sys
