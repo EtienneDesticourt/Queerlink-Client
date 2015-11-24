7b6d0c905927dc597fc03749bb0867783ccdd8f3
EtienneDesticourt <etienne.desticourt@gmail.com>
2015-11-24 10:35:59 +0100

    Clean up

--- queerlink.py
+++ queerlink.py
@@ -1,28 +1,24 @@
-import urllib.request
+import urllib.request, json
 from urllib.error import HTTPError
-import json
 
 HOST = "http://hemochro.me"
 
 def fetchAndParse(request):
     response = urllib.request.urlopen(request)   
-    data = response.read()
-    strData = data.decode('ascii')
-    jsonData = json.loads(strData)
+    data = response.read().decode('utf-8')
+    jsonData = json.loads(data)
     if jsonData['status'] == 'error':
         raise HTTPError(request.get_full_url(), 400, jsonData['data'], None, None)
     return jsonData['data']
 
 def shorten(url, host=HOST):
     "Takes a url and returns a shortened url."
-    #Request shortened url
     fullUrl = host + "/shorten/json?url=" + url
     request = urllib.request.Request(fullUrl, method='PUT')
     return fetchAndParse(request)
 
 def expand(urlId, host=HOST):
     "Takes the ID of a shortened url and retrieves the original."
-    #Request shortened url
     fullUrl = host + "/expand/json/" + urlId
     request = urllib.request.Request(fullUrl, method='GET')
     return fetchAndParse(request)
@@ -30,7 +26,6 @@ def expand(urlId, host=HOST):
 if __name__ == "__main__":
     import sys
     args = sys.argv
-    
     
     if len(args) == 2 and args[1] == "help":
         print("To shorten a url use -shorten url.\nTo expand a url use -expand id.")
